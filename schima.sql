135e808728ae1947214f1fcec08b204f26f76152
Juliana Hernandez <juliana88h@gmail.com>
2020-07-07 20:40:57 -0700

    adding js, jason, and sql files

---
+++ schima.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    stock_quantity INT NOT NULL,
+    PRIMARY KEY(item_id)
+);
+
+INSERT into products (product_name,department_name,price,stock_quantity) 
+VALUES ("chapstick","cosmetics",2.50,10)
